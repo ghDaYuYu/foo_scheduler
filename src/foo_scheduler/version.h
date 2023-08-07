0ce4378b61c7098399a96bee190fa48f1a3a49c7
fooDayuyu@gmail.com <foodayuyu@gmail.com>
2023-08-07 12:47:28 +0100

    v4.19.4

--- src/foo_scheduler/version.h
+++ src/foo_scheduler/version.h
@@ -4,7 +4,7 @@
 
 #define COMPONENT_VERSION_MAJOR 4
 #define COMPONENT_VERSION_MINOR 19
-#define COMPONENT_VERSION_PATCH 3
+#define COMPONENT_VERSION_PATCH 4
 #define COMPONENT_VERSION_SUB_PATCH 0
 
 #define MAKE_STRING(text) #text
@@ -47,6 +47,13 @@ PLUGIN_NAME"\n\n" \
 "\n" \
 "Changelog:\n" \
   "\n" \
+  "= 4.19.4\n" \
+  "* Fixed single track playlists 'Wait until 1 track played'.\n" \
+  "* Fixed status window item updates.\n" \
+  "* Fixed restore status window screen position.\n" \
+  "* Added 'Run' menu item to context menu.\n" \
+  "* Added 'End of Files' option to 'Wait until N track played'.\n" \
+  "\n" \
   "= 4.19.3\n" \
   "* DPI safe Status window placement.\n" \
   "* Added fb2k SDK and target version info to About component.\n" \
@@ -61,7 +68,7 @@ PLUGIN_NAME"\n\n" \
   "* Added date/time event context menu (timer initializer).\n" \
   "* Added dark mode.\n" \
   "\n" \
-	"= 4.19\n" \
+  "= 4.19\n" \
   "* Added 'Restart after completion' option to action list.\n" \
   "* Added 'Stop all action lists but this' action.\n" \
   "\n" \
@@ -70,8 +77,8 @@ PLUGIN_NAME"\n\n" \
   "\n" \
   "= 4.17\n" \
   "* Added action list and event duplicate functionality.\n" \
-	"* Added 'Save playback state' action and updated 'Start playback' to start from saved state.\n" \
-    "\n" \
+  "* Added 'Save playback state' action and updated 'Start playback' to start from saved state.\n" \
+  "\n" \
   "= 4.14\n" \
   "* Recovered WinXP support.\n" \
   "\n" \
@@ -82,7 +89,7 @@ PLUGIN_NAME"\n\n" \
   "* Fixed wrong firing time for repeated events.\n" \
   "\n" \
   "= 4.11\n" \
-  "* \"Scheduler enabled\" checkbox has been added.\n" \
+  "* 'Scheduler enabled' checkbox has been added.\n" \
   "\n" \
   "= 4.10\n" \
   "* Removed useless stop reason in player stop event.\n" \
@@ -93,13 +100,13 @@ PLUGIN_NAME"\n\n" \
   "* Completely fixed keyboard shortcuts assignment problem.\n" \
   "\n" \
   "= 4.08\n" \
-  "* Fixed tab order in \"Wait until N tracks played\" configuration dialog.\n" \
+  "* Fixed tab order in 'Wait until N tracks played' configuration dialog.\n" \
   "* Fixed a bug with menu items, when keyboard shortcuts couldn't be assigned to them correctly. Please recreate your menu item events!\n" \
   "\n" \
   "= 4.07\n" \
-  "* Added a new command \"Stop all action lists\" to the status window and to the main menu.\n" \
+  "* Added a new command 'Stop all action lists' to the status window and to the main menu.\n" \
   "* Header restyling to conform the style of preferences page.\n" \
-  "* New action \"Wait until N tracks played\".\n" \
+  "* New action 'Wait until N tracks played'.\n" \
   "\n" \
   "= 4.06\n" \
   "* Next/previous track actions added.\n" \
@@ -115,29 +122,29 @@ PLUGIN_NAME"\n\n" \
   "* Bug fix release.\n" \
   "\n" \
   "= 4.02\n" \
-  "* Added more detailed config of \"On playback stop\" event.\n" \
-  "* Remaining time of \"Delay\" and \"Set volume\" actions is now displayed in status window.\n" \
+  "* Added more detailed config of 'On playback stop' event.\n" \
+  "* Remaining time of 'Delay' and 'Set volume' actions is now displayed in status window.\n" \
   "\n" \
   "= 4.01\n" \
   "* Added keyboard context menu invocation in events list view and action lists view.\n" \
   "* Volume fade out bug fix.\n" \
   "* Volume fade in/out now uses logarithmic scale.\n" \
   "* Final action combo-box has been returned in date/time event.\n" \
-  "* New \"Mute/unmute\" action.\n" \
+  "* New 'Mute/unmute' action.\n" \
   "\n" \
   "= 4.0\n" \
   "The plugin is rewritten from scratch. Short list of changes:\n" \
-  "* New \"OK, Cancel, Apply\" preferences page style, introduced in fb2k 1.0.\n" \
-  "* \"Reset page\" button works correct now.\n" \
+  "* New 'OK, Cancel, Apply' preferences page style, introduced in fb2k 1.0.\n" \
+  "* 'Reset page' button works correct now.\n" \
   "* Scheduler isn't stopped while its preferences page is opened. It's running all the time.\n" \
   "* Added new status window, where you can see pending date/time events and manage running action lists.\n" \
   "* Fixed crash with concurrently running action lists.\n" \
   "* Fixed incorrect handling of date/time events in some cases.\n" \
-  "* Date/time events: completely new configuration dialog, added title support, \"remove/disable event\" option removed.\n" \
+  "* Date/time events: completely new configuration dialog, added title support, 'remove/disable event' option removed.\n" \
   "* No more annoying message boxes, now only balloons are used.\n" \
   "* Menus, treeviews and listviews use native Windows Vista/7 style.\n" \
   "* New player events: on playback start, stop, pause, unpause, track change.\n" \
   "* New actions: Pause/Unpause, Change playlist, Launch application.\n" \
-  "* For \"Delay\" and \"Set volume\" actions duration units can be selected.\n" \
+  "* For 'Delay' and 'Set volume' actions duration units can be selected.\n" \
   "* New File/Scheduler menu.\n" \
   "* No more configuration reset during version upgrade.\n"
